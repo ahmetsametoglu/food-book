3e8d01ca55642a5bc11ac637a83e26ce5b3ea3c2
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-20 00:50:42 +0200

    feat: scan-barcode implemented to home screen

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {Fragment, useState} from 'react';
+import {View, Text, StyleSheet, ViewStyle, TouchableHighlight} from 'react-native';
+import {colors} from '../utils/helper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import ScanBarcode from '../components/scan-barcode';
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+  const [scanProduct, setScanProduct] = useState(false);
+  const [lastScannedBarcode, setLastScannedBarcode] = useState('');
+
+  const handleStartScanProduct = () => {
+    setScanProduct(true);
+  };
+
+  const handleReadBarcode = (code: string) => {
+    setScanProduct(false);
+    console.log('barcode:', code);
+    setLastScannedBarcode(!!code ? code : '');
+
+    // TODO: check is product exist
+  };
+
+  const defaultView = (
+    <TouchableHighlight
+      underlayColor={colors.secondary}
+      style={[styles.centralize, {backgroundColor: colors.primary}]}
+      onPress={handleStartScanProduct}>
+      <Fragment>
+        <Icon name="barcode-scan" size={120} color={colors.light} />
+        <Text style={{color: colors.light, fontSize: 20, marginTop: 10}}>
+          tab to screen for scan product
+        </Text>
+      </Fragment>
+    </TouchableHighlight>
   );
+
+  return scanProduct ? <ScanBarcode handleReadBarcode={handleReadBarcode} /> : defaultView;
 };
 
 export default Home;
+
+const styles = StyleSheet.create<{centralize: ViewStyle}>({
+  centralize: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+});
