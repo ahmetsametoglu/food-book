b9a81a049f6c146192d008761717eb6751822f72
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-19 23:53:42 +0200

    feat: home screen and splash-screen implemented to app.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-import React, {Fragment} from 'react';
-import {StatusBar, View, Text} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React, {Fragment, useState} from 'react';
+import {StatusBar} from 'react-native';
+import Home from './src/screens/home';
+import SplashScreen from './src/components/splash-screen';
 
 const App = () => {
+  const splashScreen = <SplashScreen />;
+  setTimeout(() => {
+    const homeContent = <Home />;
+    setMainContent(homeContent);
+  }, 1000);
+
+  const [mainContent, setMainContent] = useState(splashScreen);
+
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#007991',
-        }}>
-        <Icon name="food" size={120} color="#BAD6C2" />
-        <Text style={{color: '#BAD6C2', fontSize: 40}}>food book</Text>
-      </View>
+      {mainContent}
     </Fragment>
   );
 };
