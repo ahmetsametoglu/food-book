ee4947efeb75295a0fb0213ef68a9db4fcf71b4f
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-20 01:15:33 +0200

    feat: user-store created

---
+++ src/store/user-store.ts
@@ -0,0 +1,29 @@
+import { ActionType } from './action-types';
+import { Reducer, Dispatch } from 'react';
+
+export type UserStateType = {};
+
+export type UserActionType = {
+  type: ActionType;
+  payload?: UserPayloadType;
+};
+
+type UserPayloadType = {};
+
+export class UserAction {
+  constructor(private dispatch: Dispatch<UserActionType>) {}
+}
+
+export const initialUserState: UserStateType = {};
+
+export const UserReducer: Reducer<UserStateType, UserActionType> = (
+  state = initialUserState,
+  action,
+) => {
+  console.log(`[User Reducer]: ${action.type}`);
+
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
