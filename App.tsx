16fa78b4547d4612e29890b3a422f03e0f7f2430
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-20 01:21:37 +0200

    feat: ReduxContextProvider implemented to app.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,29 @@
 import React, {Fragment, useState} from 'react';
-import {StatusBar} from 'react-native';
+import {StatusBar, View} from 'react-native';
 import Home from './src/screens/home';
 import SplashScreen from './src/components/splash-screen';
+import ReduxContextProvider from './src/context/redux-context';
 
 const App = () => {
   const splashScreen = <SplashScreen />;
   setTimeout(() => {
-    const homeContent = <Home />;
+    const homeContent = (
+      <View style={{flex: 1}}>
+        <Home />
+      </View>
+    );
     setMainContent(homeContent);
   }, 1000);
 
   const [mainContent, setMainContent] = useState(splashScreen);
 
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      {mainContent}
-    </Fragment>
+    <ReduxContextProvider>
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        {mainContent}
+      </Fragment>
+    </ReduxContextProvider>
   );
 };
 
