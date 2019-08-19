fee9e7b77d189485476a9f4b3b9dad6e6db8e36d
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-19 23:39:57 +0200

    feat: colors added

---
+++ src/utils/helper.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#007991',
+  secondary: '#409985',
+  third: '#BAD6C2',
+  error: '#C42847',
+  success: '#2D8652',
+  light: '#BAD6C2',
+  dark: '#202A4F',
+  solid: '#E8D784',
+  loadingBackgroundColor: '#B3E5C8',
+};
