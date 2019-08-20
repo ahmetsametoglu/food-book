12d62cbd3e8dba47a9c5a4728104ac12a2a62d0b
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-20 23:50:28 +0200

    feat:  loading hoc implemented

--- App.tsx
+++ App.tsx
@@ -1,29 +1,34 @@
-import React, {Fragment, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {StatusBar, View} from 'react-native';
 import Home from './src/screens/home';
 import SplashScreen from './src/components/splash-screen';
 import ReduxContextProvider from './src/context/redux-context';
+import Loading from './src/components/hocs/loading';
 
 const App = () => {
   const splashScreen = <SplashScreen />;
-  setTimeout(() => {
-    const homeContent = (
-      <View style={{flex: 1}}>
+
+  const homeContent = (
+    <ReduxContextProvider>
+      <Loading>
         <Home />
-      </View>
-    );
-    setMainContent(homeContent);
-  }, 1000);
+      </Loading>
+    </ReduxContextProvider>
+  );
 
   const [mainContent, setMainContent] = useState(splashScreen);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setMainContent(homeContent);
+    }, 1000);
+  }, []);
+
   return (
-    <ReduxContextProvider>
-      <Fragment>
-        <StatusBar barStyle="dark-content" />
-        {mainContent}
-      </Fragment>
-    </ReduxContextProvider>
+    <View style={{flex: 1}}>
+      <StatusBar barStyle="dark-content" />
+      {mainContent}
+    </View>
   );
 };
 
