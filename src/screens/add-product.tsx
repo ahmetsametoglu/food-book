1491846987c17e6a6abdacbb388060d300628c17
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-21 11:56:47 +0200

    feat: add-product screen created

---
+++ src/screens/add-product.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AddProduct = () => {
+  return (
+    <View>
+      <Text>Add Product</Text>
+    </View>
+  );
+};
+
+export default AddProduct;
