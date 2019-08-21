5979d437bf4735b7d2f236160ab0bda65a2b8840
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-21 11:55:53 +0200

    feat: stack-nav component created

---
+++ src/navigation/stack-nav.tsx
@@ -0,0 +1,32 @@
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+import {colors} from '../utils/helper';
+import Home from '../screens/home';
+import AddProduct from '../screens/add-product';
+
+const StackNav = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        header: null,
+      },
+    },
+    AddProduct: {
+      screen: AddProduct,
+      navigationOptions: ({}) => ({}),
+    },
+  },
+  {
+    defaultNavigationOptions: {
+      headerTintColor: colors.white,
+      headerStyle: {
+        height: 40,
+        backgroundColor: colors.primary,
+        borderBottomColor: colors.secondary,
+      },
+      headerBackTitle: null,
+    },
+  },
+);
+
+export default createAppContainer(StackNav);
