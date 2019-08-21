6ab38330c5206f61eb9b258145fcc47a5928e268
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-21 21:21:51 +0200

    feat: navigation provider implemented

--- App.tsx
+++ App.tsx
@@ -4,16 +4,19 @@ import SplashScreen from './src/components/splash-screen';
 import ReduxContextProvider from './src/context/redux-context';
 import Loading from './src/components/hocs/loading';
 import StackNav from './src/navigation/stack-nav';
+import NavigationContextProvider from './src/context/navigation-context';
 
 const App = () => {
   const splashScreen = <SplashScreen />;
 
   const homeContent = (
-    <ReduxContextProvider>
-      <Loading>
-        <StackNav />
-      </Loading>
-    </ReduxContextProvider>
+    <NavigationContextProvider>
+      <ReduxContextProvider>
+        <Loading>
+          <StackNav />
+        </Loading>
+      </ReduxContextProvider>
+    </NavigationContextProvider>
   );
 
   const [mainContent, setMainContent] = useState(splashScreen);
