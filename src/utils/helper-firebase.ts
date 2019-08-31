0634de7f6659ce197ed2a2d15d5b329ccc288e9e
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-31 04:54:59 +0200

    feat: some functions added
    recognizeText, subscribeAuth, signInAnonymously

--- src/utils/helper-firebase.ts
+++ src/utils/helper-firebase.ts
@@ -1,14 +1,61 @@
-import firebase, {storage} from 'react-native-firebase';
+import storage from '@react-native-firebase/storage';
+import vision from '@react-native-firebase/ml-vision';
+import auth from '@react-native-firebase/auth';
 
-export const uploadImage = (path: string) => {
-  console.log(firebase.app());
-  try {
+const uploadImage = (path: string, imageName: string): Promise<string> => {
+  return new Promise((res, rej) => {
     storage()
-      .ref(`tutorials/images/test`)
+      .ref(`/images/${imageName}`)
       .putFile(path)
-      .then(result => console.log(result))
-      .catch(err => console.error(err));
+      .on('state_changed', taskSnapshot => {
+        if (taskSnapshot.state === 'success') {
+          res(taskSnapshot.ref.getDownloadURL());
+        } else if (taskSnapshot.state !== 'running') {
+          rej();
+        }
+      });
+  });
+};
+
+const recognizeText = async (imagePath: string) => {
+  console.log('text recognizing...');
+
+  try {
+    const result = await vision().textRecognizerProcessImage(imagePath);
+    return result.text;
   } catch (error) {
     console.log(error);
   }
+};
+
+const subscribeAuth = () => {
+  const unSubscribe = auth().onAuthStateChanged(res => {
+    console.log('onAuthStateChanged:', res);
+  });
+  return unSubscribe;
+};
+
+const signInAnonymously = async () => {
+  try {
+    await auth()
+      .signInAnonymously()
+      .then(res => {
+        console.log('signInAnonymously:', res);
+      })
+      .catch(err => {
+        console.log('signInAnonymously err:', err);
+      });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const authHelper = {
+  subscribeAuth,
+  signInAnonymously,
+};
+
+export const firebaseHelper = {
+  uploadImage,
+  recognizeText,
 };
