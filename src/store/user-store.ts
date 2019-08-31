864e35f6303754c1b0e4eea4071e0f5883cc2b93
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-31 22:09:43 +0200

    feat: setUser action created

--- src/store/user-store.ts
+++ src/store/user-store.ts
@@ -1,20 +1,30 @@
 import {ActionType} from './action-types';
 import {Reducer, Dispatch} from 'react';
+import {User} from '../models/user';
 
-export type UserStateType = {};
+export type UserStateType = {
+  user?: User;
+};
 
 export type UserActionType = {
   type: ActionType;
-  payload?: UserPayloadType;
+  payload: UserPayloadType;
 };
 
-type UserPayloadType = {};
+type UserPayloadType = {user?: User};
 
 export class UserService {
   constructor(private dispatch: Dispatch<UserActionType>) {}
+
+  setUser(user?: User) {
+    this.dispatch({
+      payload: {user: user},
+      type: 'SetUser',
+    });
+  }
 }
 
-export const initialUserState: UserStateType = {};
+export const initialUserState: UserStateType = {user: undefined};
 
 export const UserReducer: Reducer<UserStateType, UserActionType> = (
   state = initialUserState,
@@ -23,6 +33,10 @@ export const UserReducer: Reducer<UserStateType, UserActionType> = (
   console.log(`[User Reducer]: ${action.type}`);
 
   switch (action.type) {
+    case 'SetUser':
+      const user = action.payload.user;
+      return {...state, user};
+
     default:
       return {...state};
   }
